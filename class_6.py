048cb9529a511aeb6f62f5e857c552efbc05b27e
Pawan chaudhary <manojchaudhary3168@gmail.com>
2022-12-13 07:44:04 +0545

    added

--- class_6.py
+++ class_6.py
@@ -49,16 +49,3 @@
 # b=[item.upper() for item in mmy if mmy.index(item) % 2 ==0]
 # print(b)
 
-X = [[1,8,4],
-    [4 ,5,6],
-    [5 ,8,9]]
-
-Y = [[15,8,3],
-    [6,4,7],
-    [4,6,9]]
-
-result = [[0,0,0],
-         [0,0,0],
-         [0,0,0]]
-p=[result[i][j]=result[i][j]+X[i][k]*Y[k][j] for i in range(len(X)) for j in range(len(Y[0])) for k in range(len(Y))]
-print(result)
