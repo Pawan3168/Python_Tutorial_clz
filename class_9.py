1d371af6bd133c10fd8220dfce89e139178cd3c1
Pawan chaudhary <manojchaudhary3168@gmail.com>
2022-12-16 08:30:34 +0545

    nested Dictionary

---
+++ class_9.py
@@ -0,0 +1,40 @@
+# # addition of items in dictionary
+# classmate = {"Manoj":1, "Pawan":2}
+# print(classmate)
+# classmate["sumit"] = 3  #added the item in dictionary 
+# print(classmate)
+# classmate["Pawan"] = 4 #changes the value of key pawan
+# print(classmate)
+# print(classmate.items()) #it shows the items inside the dictionary
+# print(classmate["Manoj"] == 1) #it shows the value of key is true or false
+# print(classmate["Manoj"] == 12) #false
+# classmate.pop("sumit") #it delete the key and value from dictionary
+# print(classmate)
+# print(len(classmate)) #length of dictionary
+# classmate["name"] = "Ram"
+# print(classmate)
+# classmate.popitem() #it pop out the last value and key 
+# print("pop the last ram: ",classmate)
+# print(classmate.get("Manoj")) #getting the value of key manoj
+# classmate["Benchmate"] = {"sanket", "sagarmatha", "anything"} #nested dictionary dictionary inside dictionary
+# print(classmate)
+sagarmatha = {
+        "Education Department": {
+                "Computer Science": {
+                        "HOD":"Bharat Bhatta", 
+                        "no_of_students": 100
+                },
+                "Civil": {
+                        "HOD":"Sudip Lamsal",
+                        "no_of_students": 200
+                }
+        },
+        "Admin Department": {
+                "Accounts": {
+                        "HOD":"Chaturbhuj Nepali",
+                        "no_of_students": 50
+                }
+        }
+}
+print(sagarmatha["Education Department"]["Computer Science"]["HOD"]) #printed the nested dictionary
+print(sagarmatha["Education Department"]["Civil"]["HOD"])
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,4 @@
-print(tup[-1])
-# for i in tup:
-#     print(i)
-# # list can be modified i.e can be remove and insert in list
-# dumb=[1,5]
-# dumb.insert(1,'apple') # inside the bracket first indicates the position where to insert the items
-# print(dumb)
-
-# dumb.append(4444) #it added at last of list
-# print(dumb)
+    # "Civil" :{
+    #     "HOD" : "I don't know",
+    #     "Number of students ": "May be 48"
+    # }
