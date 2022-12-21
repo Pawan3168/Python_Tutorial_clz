b555d130884cff6f5afa131079440f6f1ccea5ae
Pawan chaudhary <manojchaudhary3168@gmail.com>
2022-12-21 09:19:30 +0545

    add

---
+++ class_12.py
@@ -0,0 +1,7 @@
+# to activate streamlit write Scripts\activate
+import streamlit as st
+
+
+st.title('HELLO!! TO MY WORLD')
+# pip freeze>requirementa.txt made for package already installed in your computer and this will list out all package in txt file
+# pip install -r requirementa.txt this used by other to install all package you have used 
